262201eb63b61a28a751391c96b430c75c2e87f0
Krzysztof Urbas <krzysu@interia.pl>
2012-05-07 13:13:08 +0200

    add new build of demo page

--- javascripts/auto_scroller.js
+++ javascripts/auto_scroller.js
@@ -1,1 +1,1 @@
-((function(){CaseStudy.AutoScroller=function(){function a(){}return a.prototype.scrollTo=function(a){var b;return b=$(a).offset().top-100,$("html:not(:animated), body:not(:animated)").animate({scrollTop:b},"normal")},a}()})).call(this)
+((function(){CaseStudy.AutoScroller=function(){function a(){}return a.scrollTo=function(a,b){var c;return c=$(a).offset().top-50,$("html:not(:animated), body:not(:animated)").animate({scrollTop:c},{duration:"normal",complete:function(){if(b!=null&&typeof b=="function")return b()}})},a}()})).call(this)
